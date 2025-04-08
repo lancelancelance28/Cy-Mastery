f3d800e9622050653e103d0bd2f64ed2c011b97b
Darryll Lance C. Montenegro <lancem28.dlm@gmail.com>
2025-04-08 16:40:20 +0800

    Feat/Ecom Flow

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,19 @@
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "2aidwb",
+  reporter:"cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Regression Testing Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,49 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('auth', (username, password) => {
+    cy.visit('https://www.saucedemo.com/', {timeout: 240000})
+      cy.get('[data-test="username"]').type(username)
+      cy.get('[data-test="password"]').type(password)
+      cy.get('[data-test="login-button"]').click()
+}); 
+
+Cypress.Commands.add('addToCart', (productTestId) => {
+    // Add product to the cart
+    cy.get(`[data-test="${productTestId}"]`)
+      .should('be.visible')
+      .click();
+      cy.get('.shopping_cart_badge').should('contain', '1')
+ 
+     // Optionally, navigate to the cart and verify item is listed
+     cy.get('.shopping_cart_link').click()
+     cy.url().should('include', '/cart.html')
+     cy.get('.cart_item').should('have.length', 1)
+     cy.get('.inventory_item_name').should('contain', 'Sauce Labs Backpack')
+  });
+
+  Cypress.Commands.add('checkOut', (firstName, lastName, postalCode) => {
+   
+    cy.get('.shopping_cart_link').click()
+    cy.url().should('include', '/cart.html')
+
+    cy.get('.checkout_button').click()
+
+    cy.url().should('include', '/checkout-step-one.html')
+    cy.get('[data-test="firstName"]').type(firstName)
+    cy.get('[data-test="lastName"]').type(lastName)
+    cy.get('[data-test="postalCode"]').type(postalCode)
+    cy.get('[data-test="continue"]').click()
+
+    
+    cy.url().should('include', '/checkout-step-two.html')
+    cy.get('.summary_info').should('be.visible')
+    cy.get('.summary_total_label').should('contain', 'Total:') 
+    
+
+    cy.get('[data-test="finish"]').click()
+    
+        cy.url().should('include', '/checkout-complete.html')
+        cy.get('.complete-header').should('contain', 'Thank you for your order!')
+  });
